d6d541ec41e4364346ce6266af3d273cc1fbaa47
Gustav Burchardt <gustavburchardt@gmail.com>
2019-10-06 11:50:09 +0200

    Change deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 npm run build
 git add dist
 git commit -m "Deploy to gh-pages"
-git subtree push --prefix dist origin gh-pages
+git remote add pages git@github.com:gustavgb/gustavgb.github.io.git
+git subtree push --prefix dist pages master
